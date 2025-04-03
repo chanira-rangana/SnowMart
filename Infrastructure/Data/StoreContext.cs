7a57696de0a5b83bae2c27aa766cefca91b13040
chanira-rangana <chanirapannala@gmail.com>
2025-04-03 17:28:08 +0530

    create product crud

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,14 +1,17 @@
 ﻿using Core.Entities;
+using Infrastructure.Config;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
-
-public class StoreContext : DbContext
+public class StoreContext(DbContextOptions options) : DbContext(options)
 {
-    public StoreContext(DbContextOptions options) : base(options)
-    {
-    }
-    
-    public DbSet<Product> Products { get; set; } 
+    public DbSet<Product> Products { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+    }
 }
+
+
